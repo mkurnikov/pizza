9d7ba5aacdfacf2e8e6eb22053a170f932b104f3
mkurnikov <maxim.kurnikov@gmail.com>
2015-03-23 23:07:13 +0300

    exchange

--- server.sh
+++ server.sh
@@ -1,3 +1,4 @@
+cd ../
 sudo apt-get install git postgresql
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
@@ -6,7 +7,6 @@ sudo apt-get install oracle-java7-set-default
 wget http://mirror.tcpdiag.net/apache/tomcat/tomcat-7/v7.0.59/bin/apache-tomcat-7.0.59.tar.gz
 tar xvf apache-tomcat-7.0.59.tar.gz
 sh apache-tomcat-7.0.59//bin/startup.sh
-git clone https://github.com/mkurnikov/pizza.git
 cd pizza/
 cp tomcat-conf/* ../apache-tomcat-7.0.59/conf/
 sudo -u postgres psql -f src/main/resources/database/db.sql
